e0316535d262ac5bcff8e1ea8caa79ae1599fd79
Dr. Derek Austin [aka dj D-REK from Richmond, VA] <53202242+djD-REK@users.noreply.github.com>
2019-08-30 11:00:20 -0400

    Added All permutations of falsy comparisons in JavaScript

--- src/index.js
+++ src/index.js
@@ -52,3 +52,49 @@ console.log(Number("7") - 7 === Number("0"));
 console.log("7" - 7 == "0");
 console.log("7" - 7 === "0");
 // Results: true, true, true, true, false
+
+console.log("Question 4");
+
+// All permutations of falsy comparisons in JavaScript by Dr. Derek Austin 🥳
+console.log(false == false); // true
+console.log(false === false); // true
+console.log(false == 0); // true -- false, 0, and "" are loosely equal
+console.log(false === 0); // false
+console.log(false == ""); // true -- false, 0, and "" are loosely equal
+console.log(false === ""); // false
+console.log(false == null); // false
+console.log(false === null); // false
+console.log(false == undefined); // false
+console.log(false === undefined); // false
+console.log(false == NaN); // false
+console.log(false === NaN); // false
+console.log(0 == 0); // true
+console.log(0 === 0); // true
+console.log(0 == ""); // true -- false, 0, and "" are loosely equal
+console.log(0 === ""); // false
+console.log(0 == null); // false
+console.log(0 === null); // false
+console.log(0 == undefined); // false
+console.log(0 === undefined); // false
+console.log(0 == NaN); // false
+console.log(0 === NaN); // false
+console.log("" == ""); // true
+console.log("" === ""); // true
+console.log("" == null); // false
+console.log("" === null); // false
+console.log("" == undefined); // false
+console.log("" === undefined); // false
+console.log("" == NaN); // false
+console.log("" === NaN); // false
+console.log(null == null); // true
+console.log(null === null); // true
+console.log(null == undefined); // true -- null loosely equals undefined
+console.log(null === undefined); // false
+console.log(null == NaN); // false
+console.log(null === NaN); // false
+console.log(undefined == undefined); // true
+console.log(undefined === undefined); // true
+console.log(undefined == NaN); // false
+console.log(undefined === NaN); // false
+console.log(NaN == NaN); // false -- NaN nothing equals NaN, and
+console.log(NaN === NaN); // false -- NaN doesn't equal itself
